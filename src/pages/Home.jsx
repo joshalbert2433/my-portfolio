ac97a4f7782b5fb15c51f551129063bdd6b6bbe2
joshalbert2433 <joshua.albert.villanueva@gmail.com>
2023-02-04 00:00:58 +0800

    update UI

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Wave from "react-wavify";
 import Header from "../components/Header";
 import { TypeAnimation } from "react-type-animation";
@@ -7,17 +7,32 @@ import FileText from "../assets/file-text.svg";
 import { MdLocationPin } from "react-icons/md";
 import { FiAtSign } from "react-icons/fi";
 import { FaBeer } from "react-icons/fa";
+import MyPic from "../assets/me.jpg";
 
 function Home() {
+	const [isMobile, setIsMobile] = useState(false);
+
+	useEffect(() => {
+		window.addEventListener("resize", handleResize);
+	});
+
+	const handleResize = () => {
+		if (window.innerWidth < 720) {
+			setIsMobile(true);
+		} else {
+			setIsMobile(false);
+		}
+	};
+
 	return (
 		<>
-			<section className="min-h-screen flex flex-col">
+			<section className="min-h-screen flex flex-col ">
 				<Header />
 
-				<div className="container  flex flex-grow flex-col justify-center p-4 mx-auto sm:py-12 lg:py-24 lg:flex-row lg:justify-between">
-					<div className="flex flex-col justify-center lg:px-4 text-center rounded-sm lg:max-w-md xl:max-w-lg lg:text-left">
+				<div className="container  flex flex-grow flex-col justify-center items-center p-4 mx-auto sm:py-12 lg:py-24 lg:flex-row lg:justify-between">
+					<div className="flex flex-col justify-center lg:px-4 text-center lg:max-w-md xl:max-w-2xl lg:text-left">
 						<h1 className="text-4xl text-primary font-bold leading-none sm:text-6xl">
-							Hi, I am Joshua
+							Hi, I am Joshua Albert Villanueva
 						</h1>
 						<div className="my-6 text-base sm:mb-4">
 							<TypeAnimation
@@ -29,7 +44,7 @@ function Home() {
 									"Back-End Web Developer",
 									2000, // Types 'Three' without deleting 'Two'
 									() => {
-										console.log("Done typing!"); // Place optional callbacks anywhere in the array
+										// console.log("Done typing!"); // Place optional callbacks anywhere in the array
 									},
 								]}
 								wrapper="div"
@@ -50,16 +65,32 @@ function Home() {
 							joshua.albert.villanueva@gmail.com
 						</p>
 					</div>
+					<div className="flex items-center justify-center p-6 mt-8 lg:mt-0 h-72 sm:h-80 lg:h-96 xl:h-112 2xl:h-128">
+						<img
+							src={MyPic}
+							alt="my pic"
+							className="h-72 sm:h-80 lg:h-96 xl:h-112 2xl:h-128 rounded-full object-cover border-8 border-base-300"
+						/>
+					</div>
 				</div>
 			</section>
-			<Wave mask="url(#mask)" fill="#1277b0">
+			<Wave
+				mask="url(#mask)"
+				fill="#2F5496"
+				options={{
+					height: 40,
+					amplitude: 20,
+					speed: 0.35,
+					points: isMobile ? 2 : 7,
+				}}
+			>
 				<defs>
 					<linearGradient
 						id="gradient"
 						gradientTransform="rotate(90)"
 					>
-						<stop offset="0" stopColor="white" />
-						<stop offset="0.5" stopColor="black" />
+						<stop offset="0" stopColor="black" />
+						<stop offset="0.5" stopColor="white" />
 					</linearGradient>
 					<mask id="mask">
 						<rect
@@ -73,7 +104,39 @@ function Home() {
 				</defs>
 			</Wave>
 
-			<section className="min-h-screen"></section>
+			<section className="min-h-screen bg-base-200 -mt-4"></section>
+			<div className="bg-base-200">
+				<Wave
+					mask="url(#mask)"
+					fill="#2A4B87"
+					options={{
+						height: 40,
+						amplitude: 20,
+						speed: 0.35,
+						points: isMobile ? 2 : 7,
+					}}
+				>
+					<defs>
+						<linearGradient
+							id="gradient"
+							gradientTransform="rotate(90)"
+						>
+							<stop offset="0" stopColor="black" />
+							<stop offset="0.5" stopColor="white" />
+						</linearGradient>
+						<mask id="mask">
+							<rect
+								x="0"
+								y="0"
+								width="100%"
+								height="200"
+								fill="url(#gradient)"
+							/>
+						</mask>
+					</defs>
+				</Wave>
+				<section className="min-h-screen bg-base-300 -mt-4"></section>
+			</div>
 		</>
 	);
 }
